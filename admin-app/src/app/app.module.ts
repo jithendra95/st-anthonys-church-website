9c2c6adcc341b492cfbc63a51f1d327bde25403e
jithendra <jithendrathenu@gmail.com>
2022-03-19 18:19:42 +0530

    Added Admin App

---
+++ admin-app/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from '../shared/material.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdminConsoleComponent } from './admin-console/admin-console.component';
+import { environment } from 'src/environments/environment';
+import { GeneralComponent } from './components/general/general.component';
+import { MassComponent } from './components/mass/mass.component';
+import { AnnouncementComponent } from './components/announcement/announcement.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminConsoleComponent,
+    GeneralComponent,
+    MassComponent,
+    AnnouncementComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ admin-app/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCphmOzPdNWcWmXQyFT7rn8tCtafp1leOE',
+    authDomain: 'church-web-mahar.firebaseapp.com',
+    projectId: 'church-web-mahar',
+    storageBucket: 'church-web-mahar.appspot.com',
+    messagingSenderId: '1028660436145',
+    appId: '1:1028660436145:web:4cecb6a1b1af6c2c575690',
+    measurementId: 'G-577L1YT774',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
